42e3a772e04051ad8e2bb137cf3eeccd50d63787
“Shaina <shainamish@gmail.com>
2022-01-25 21:52:56 -0800

    Fixed Swap Mechanic
    Swap mechanic was turning the character off but not turning the other character on

---
+++ FinalMessagePrototype/Assets/Scripts/dogMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject dogon;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+
+    // Awake is called after all objects are initialized. Called in random order
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); // will look for a component on this GameObject (what the script is attached to) of type Rigidbody2D
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && dogon.activeSelf == true) {
+            dogon.SetActive(false);
+        } 
+        else if (Input.GetKeyDown("space") && dogon.activeSelf == false) {
+            dogon.SetActive(true);
+        }
+            
+        if (dogon.activeSelf == true)
+        {
+            // Get player inputs
+            processInputs();
+
+            // Animate
+            animate();
+
+            // Move
+            move();
+        }
+    }
+
+    private void move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+
+    private void animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            flipChara();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            flipChara();
+        }
+    }
+
+    private void processInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); // from -1 to 1
+    }
+
+    private void flipChara()
+    {
+        facingRight = !facingRight; //inverse boolean
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
