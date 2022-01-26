64efebfd5828f97ba58a34885c9a4fa5891ad700
“Shaina <shainamish@gmail.com>
2022-01-26 03:39:27 -0800

    Transition Level + Reasoning Setup

---
+++ FinalMessagePrototype/Assets/Scripts/Tutorial/DoorMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMove : MonoBehaviour
+{
+    public GameObject door;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // osilates the door up and down
+        float y = Mathf.PingPong(Time.time * speed, 1) * 3 - 3;
+        door.transform.position = new Vector3(0, y, 0);
+    }
+}
