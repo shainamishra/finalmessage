70322f42a0afe17d657d5a8c102a144f3c59e84c
“Shaina <shainamish@gmail.com>
2022-02-04 15:24:17 -0800

    Random tweaks

--- FinalMessagePrototype/Assets/Scripts/Tutorial/CollisionIgnore.cs
+++ FinalMessagePrototype/Assets/Scripts/Tutorial/CollisionIgnore.cs
@@ -18,6 +18,10 @@ public class CollisionIgnore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // store in a list
+        // maybe move to start or make a function to call the ignore coll
+        // do operations on the list itself
+        // catch exception and remove from list or before calling the function check if both objects are in the scene
         Physics2D.IgnoreCollision(dog.GetComponent<Collider2D>(), human.GetComponent<Collider2D>());
         Physics2D.IgnoreCollision(dog.GetComponent<Collider2D>(), object1.GetComponent<Collider2D>());
         Physics2D.IgnoreCollision(dog.GetComponent<Collider2D>(), object2.GetComponent<Collider2D>());
