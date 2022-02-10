baa9d484d9594fafe31103e6a361bf3cda284596
Chantel <chmagee@ucsc.edu>
2022-02-10 14:25:37 -0800

    four button puzzle objects placed

--- FinalMessagePrototype/Assets/Scripts/Tutorial/DoorMove.cs
+++ FinalMessagePrototype/Assets/Scripts/Tutorial/DoorMove.cs
@@ -17,7 +17,7 @@ public class DoorMove : MonoBehaviour
     void Update()
     {
         // osilates the door up and down
-        float y = Mathf.PingPong(Time.time * speed, 1) * 3 - 2;
+        float y = Mathf.PingPong(Time.time * speed, 1) * 3 - 1;
         door.transform.position = new Vector3(0, y, 0);
     }
 }
