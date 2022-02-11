a4d86a708b6164737758ee39b0a011bc03a9702d
Chantel <chmagee@ucsc.edu>
2022-02-10 19:05:07 -0800

    new BLURRY sprites

---
+++ FinalMessagePrototype/Assets/Scripts/General/Climbing.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Climbing : MonoBehaviour
+{
+    private float vertical;
+    private float speed = 3f;
+    private bool canClimb; 
+    private bool isClimbing;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (canClimb && Mathf.Abs(vertical) > 0f)
+        {
+            isClimbing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, vertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CanClimb"))
+        {
+            canClimb = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CanClimb"))
+        {
+            canClimb = false;
+            isClimbing = false;
+        }
+    }
+}
