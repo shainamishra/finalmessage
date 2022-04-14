bab563c025c018fe82e58e3e1392ff0cc7a31526
“Shaina <shainamish@gmail.com>
2022-04-14 01:36:03 -0700

    okay NOW text is actually working

--- FinalMessagePrototype/Assets/Scripts/Level Scripts/TextTrigger.cs
+++ FinalMessagePrototype/Assets/Scripts/Level Scripts/TextTrigger.cs
@@ -1,36 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class TextTrigger : MonoBehaviour
 {
     public Collider2D player; 
     public Collider2D npc; 
+    public Button start; 
+    public Button cont; 
+    public Button option1; 
+    public Button option2; 
+    public Button option3; 
+    public Button option4; 
     public GameObject canvas; 
     
     void Start()
     {
+        // hide text
         DisableCanvas();
+
+        // hide start button
+        start.gameObject.SetActive(false);
+
+        // start button listeners
+        Button startBTN = start.GetComponent<Button>();
+        startBTN.onClick.AddListener(TaskOnClick);
+
+        // continue button listeners
+        Button continueBTN = cont.GetComponent<Button>();
+        continueBTN.onClick.AddListener(TaskOnClick2);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(npc.IsTouching(player))
+        // if the player touches the NPC
+        if(player.IsTouching(npc))
         {
-            EnableCanvas();
+            // show the talk button
+            start.gameObject.SetActive(true);
         }
-        
     }
     
+    // hide the canvas
     void DisableCanvas() 
     {
         canvas.SetActive(false);
     }
 
+    // show the canvas
     void EnableCanvas() 
     {
         canvas.SetActive(true);
+    }
+
+    // when the start button is clicked
+    void TaskOnClick()
+    {
+        // hide the start button
+        start.gameObject.SetActive(false);
+        // show the text canvas
+        EnableCanvas();
+    }
+    
+    // when the start button is clicked
+    void TaskOnClick2()
+    {
+        // show the text canvas
+        option1.gameObject.SetActive(true);
+        option2.gameObject.SetActive(true);
+        option3.gameObject.SetActive(true);
+        option4.gameObject.SetActive(true);
     }
 
 }
--- FinalMessagePrototype/Assets/Scripts/Tutorial/NPCBehavior.cs
+++ FinalMessagePrototype/Assets/Scripts/Tutorial/NPCBehavior.cs
@@ -10,18 +10,18 @@ public class NPCBehavior : MonoBehaviour
 
     void Start()
     {
-        canvas.SetActive(false);
+        //canvas.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        canvas.SetActive(true);
-        npc.GetComponent<Collider2D>().enabled = false;;
-        OpenDoor();
+        //canvas.SetActive(true);
+        //npc.GetComponent<Collider2D>().enabled = false;;
+        //OpenDoor();
     }
 
     public void OpenDoor()
     {
-        GetComponent<BoxCollider2D>().isTrigger = true;
+        //GetComponent<BoxCollider2D>().isTrigger = true;
     }
 }
