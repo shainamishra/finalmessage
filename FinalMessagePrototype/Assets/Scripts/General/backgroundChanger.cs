b48bbc194f6b7e27943ef2a31c6bcba1f7224378
“Shaina <shainamish@gmail.com>
2022-05-05 00:47:01 -0700

    Update pause UI + all art should change with time now

--- FinalMessagePrototype/Assets/Scripts/General/backgroundChanger.cs
+++ FinalMessagePrototype/Assets/Scripts/General/backgroundChanger.cs
@@ -16,6 +16,16 @@ public class backgroundChanger : MonoBehaviour
     public Sprite cliff2;
     public Sprite cliff3;
 
+    // extra cliffs
+    public SpriteRenderer cliffPt2;
+
+    // extra floors
+    public SpriteRenderer floorPt2;
+    public SpriteRenderer floorPt3;
+    public SpriteRenderer floorPt4;
+    public SpriteRenderer floorPt5;
+    public SpriteRenderer floorPt6;
+
     // Update is called once per frame
     void Update()
     {
@@ -24,6 +34,14 @@ public class backgroundChanger : MonoBehaviour
             bg1.sprite = bg2;
             fg1.sprite = fg2;
             cliff1.sprite = cliff2;
+
+            cliffPt2.sprite = cliff2;
+
+            floorPt2.sprite = fg2;
+            floorPt3.sprite = fg2;
+            floorPt4.sprite = fg2;
+            floorPt5.sprite = fg2;
+            floorPt6.sprite = fg2;
         }
 
         if (TimeManager.startingTime <= 0)
@@ -31,6 +49,14 @@ public class backgroundChanger : MonoBehaviour
             bg1.sprite = bg3;
             fg1.sprite = fg3;
             cliff1.sprite = cliff3;
+
+            cliffPt2.sprite = cliff3;
+
+            floorPt2.sprite = fg3;
+            floorPt3.sprite = fg3;
+            floorPt4.sprite = fg3;
+            floorPt5.sprite = fg3;
+            floorPt6.sprite = fg3;
         }
     }
 }
