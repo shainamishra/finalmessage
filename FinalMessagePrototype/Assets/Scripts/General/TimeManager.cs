8beaeb3bd0f51a1c9f9e5fa16d01ac4bc424af78
juliannah5203 <57543226+juliannah5203@users.noreply.github.com>
2022-05-16 01:58:23 -0700

    fix the position of shadowknight according to the knight

--- FinalMessagePrototype/Assets/Scripts/General/TimeManager.cs
+++ FinalMessagePrototype/Assets/Scripts/General/TimeManager.cs
@@ -12,12 +12,16 @@ public class TimeManager : MonoBehaviour
 
     public static bool TimesUp = false;
     public GameObject shadow;
+    
 
     public Animator shadowidle;
+
+    public cameraMovement cameraMovement;
 
 
     // public Transform kpos;
     public Transform skpos;
+    public GameObject knight;
     public static float speed = 0.000000000000000000000000000000000000000000000001f;
 
     static TimeManager instance;
@@ -41,6 +45,8 @@ public class TimeManager : MonoBehaviour
       // DontDestroyOnLoad(this.gameObject);
       
         theText = GetComponent<Text>();
+        knight = GameObject.Find("PlayerPrefab");
+        cameraMovement = GameObject.Find("Main Camera").GetComponent<cameraMovement>();
         TimesUp = false;
 
 
@@ -49,6 +55,9 @@ public class TimeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        knight = GameObject.Find("PlayerPrefab");
+        cameraMovement = GameObject.Find("Main Camera").GetComponent<cameraMovement>();
+
         startingTime -= Time.deltaTime;
         float t = startingTime;
         string min = ((int) t/60).ToString();
@@ -71,20 +80,29 @@ public class TimeManager : MonoBehaviour
         if(startingTime<=0){
           startingTime=0;
           theText.text = "0:00";
+          if(cameraMovement.dogon.activeSelf == true){
+              cameraMovement.dogon.SetActive(false);
+              cameraMovement.playeron.SetActive(true);
+            }
 
             shadow.SetActive(true);
-            if(skpos.position.x>0){
+            
+            if(skpos.position.x>knight.transform.position.x+5){
 
-                // shadowMove();
                 StartCoroutine(waiter());
                 
             }
+            // skpos.SetPositionAndRotation(new Vector3(250f,-1.33f,0f),new Quaternion(0,0,0,0));
+
             TimesUp = true;
+            
         }
         
 
         if(TimesUp == true && Input.GetKeyDown(KeyCode.R)){
           // shadow.SetActive(false);
+          skpos.SetPositionAndRotation(new Vector3(250f,-1.33f,0f),new Quaternion(0,0,0,0));
+
           TimesUp = false;
           startingTime = 300;
           SceneManager.LoadScene("1StartScene");
@@ -98,12 +116,13 @@ public class TimeManager : MonoBehaviour
       // //  rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
       //   // Vector2 target = new Vector2(-1f,0f);
       //   // Vector2 path = Vector2.MoveTowards(target,skpos.position,rb.velocity.x);
- shadowidle.SetTrigger("Start");
-        if(skpos.position.x>10){
-          Vector3 path1 = Vector3.MoveTowards(new Vector3(-6f,0,0),skpos.position,speed*Time.deltaTime/100000000000);
-          skpos.Translate(path1);
+        
+        if(skpos.position.x>knight.transform.position.x+20){
+          // Vector3 path1 = Vector3.MoveTowards(new Vector3(-20f,0,0),skpos.position,speed*Time.deltaTime/100000000000);
+          skpos.Translate(new Vector3(-10,0,0));
         }
         else{
+          shadowidle.SetTrigger("Start");
           Vector3 path2 = Vector3.MoveTowards(new Vector3(-0.01f,0,0),skpos.position,speed*Time.deltaTime/100000000000);
           skpos.Translate(path2);
         }
@@ -146,7 +165,7 @@ public class TimeManager : MonoBehaviour
         // Debug.Log("Fine. I admit it. I cut in line.");
         // shadowLeave();
         // Debug.Log("flipped");
-        skpos.Translate(102,-1.3f,0);
+        skpos.SetPositionAndRotation(new Vector3(skpos.position.x,skpos.position.y,0f),new Quaternion(0,0,0,0));
         Debug.Log("Leave");
           // startingTime = 300;
           // Debug.Log("Restart");
