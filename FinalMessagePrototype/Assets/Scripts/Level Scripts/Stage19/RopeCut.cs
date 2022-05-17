2083b942b98b0d6a8bcf08bb14cb2e171db6f144
Lucas Bryant <lurbryan@ucsc.edu>
2022-05-17 13:24:09 -0700

    Fixed material glitch
    Fixed material separation problem, added some code comments

--- FinalMessagePrototype/Assets/Scripts/Level Scripts/Stage19/RopeCut.cs
+++ FinalMessagePrototype/Assets/Scripts/Level Scripts/Stage19/RopeCut.cs
@@ -1,5 +1,5 @@
 /* A simple script to make the rope an interactable object that releases the elevator.
- * It also "animates" is, by which I mean it stops rendering after a being cut.
+ * It also "animates" it, by which I mean it stops rendering after a being cut.
 */
 using System.Collections;
 using System.Collections.Generic;
@@ -7,39 +7,25 @@ using UnityEngine;
 
 public class RopeCut : MonoBehaviour
 {
-    //public GameObject strikeZone;
     public bool requires_key = false;
     public bool status;
 
-    //Collider2D strikeZoneCollider;
-    //Collider2D thisRope;
     bool is_on;
     bool condition;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        status = false;
-        strikeZone = GameObject.Find("StrikeZone").GetComponent<Collider2D>();
-        thisRope = gameObject.GetComponent<Collider2D>();
-        /*
-        if(requires_key){
-            condition = (collider.gameObject.name == "StrikeZone") && (LevelLoader.Key1 == 1);
-        }
-        else{
-            condition = (collider.gameObject.name == "StrikeZone");
-        }
-        */
         status = false;
         is_on = false;
-        //thisRope = gameObject.GetComponent<Collider2D>();
-        //strikeZoneCollider = strikeZone.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Since this script is also used on the Falchion Cord,
+        // this block makes it modular by allowing the user to 
+        // turn the key requirement on and off as needed
         if(requires_key){
             condition = is_on && (LevelLoader.Key1 == 1);
         }
@@ -51,13 +37,6 @@ public class RopeCut : MonoBehaviour
             Destroy(gameObject, 0.15f);
             status = true;
         }
-        /*
-        thisRope = gameObject.GetComponent<Collider2D>();
-        if(thisRope.IsTouching(strikeZone)){
-            Destroy(gameObject, 0.15f);
-            status = true;
-        }
-        */
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
