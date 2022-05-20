952f8a39b6ff83cc2f607f742ba0f7c969f0af6c
RedKnightM <rkatkori@ucsc.edu>
2022-05-20 16:04:42 -0700

    Started work on boulder push audio

---
+++ FinalMessagePrototype/Assets/BoulderAudio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class BoulderAudio : MonoBehaviour
+{
+    public GameObject boulder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
