fa0fb1b6c7f51b777582c096f30be50519291ffa
Lucas Bryant <lurbryan@ucsc.edu>
2022-05-24 13:54:14 -0700

    Un-cheesed 16 a bit
    Level 16 adjusted for puzzle breaking elements. The crow now sits on a small wall that prevents the player from simply vaulting the doors below, and the left side door guarding the other exit has been made taller for the same reason.

--- FinalMessagePrototype/Assets/Scripts/Level Scripts/Stage15/DropFromRopeCut.cs
+++ FinalMessagePrototype/Assets/Scripts/Level Scripts/Stage15/DropFromRopeCut.cs
@@ -14,6 +14,9 @@ public class DropFromRopeCut : MonoBehaviour
     {
         ropeCut = rope.GetComponent<RopeCut>();
         body = gameObject.GetComponent<Rigidbody2D>();
+        // I'm using both constraints and gravity because the player needs to be prevented from throwing the rock off the screen,
+        // but using *only* constraints tends to not kick in until the player collides with the object, even if gravity is on as well.
+        // So, the setting of free constraints, *then* reactivate gravity gets Unity to actually drop it on cue.
         body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         body.gravityScale = 0;
     }
@@ -22,6 +25,7 @@ public class DropFromRopeCut : MonoBehaviour
     void Update()
     {
         if(ropeCut.status){
+            // Free the constraints, then immediately lock the rotation agian to prevent it from going everywhere.
             body.constraints = RigidbodyConstraints2D.None;
             body.constraints = RigidbodyConstraints2D.FreezeRotation;
             body.gravityScale = 1;
