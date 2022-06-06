1d116ecf8b1da6ad38ac3c1f702ea8db9d428131
RedKnightM <rkatkori@ucsc.edu>
2022-06-06 14:58:11 -0700

    added pick up for falchion & bells, louder bells

--- FinalMessagePrototype/Assets/Scripts/General/CollectKeys.cs
+++ FinalMessagePrototype/Assets/Scripts/General/CollectKeys.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class CollectKeys : MonoBehaviour
 {
     public GameObject key;
+    public FMOD.Studio.EventInstance pickUpAudio;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        pickUpAudio = RuntimeManager.CreateInstance("event:/UI/Item-PickUp");
     }
 
     // Update is called once per frame
@@ -23,6 +25,9 @@ public class CollectKeys : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             this.gameObject.SetActive(false);
+            if (!AudioManager.isPlaying(pickUpAudio)) {
+                pickUpAudio.start();
+            }
             if (this.gameObject.name == "WitchKnightKey") 
             {
                 LevelLoader.Key4 = 1;
@@ -39,6 +44,8 @@ public class CollectKeys : MonoBehaviour
             {
                 LevelLoader.Key3 = 1;
             }
+
+
         }
     }
 }
