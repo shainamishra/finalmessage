8880086a5551ca6e4f5c8ae4680d19e814e764fe
“Shaina <shainamish@gmail.com>
2022-06-06 22:59:47 -0700

    Kill pause menu after summit

--- FinalMessagePrototype/Assets/Scripts/General/CanvasLoader.cs
+++ FinalMessagePrototype/Assets/Scripts/General/CanvasLoader.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CanvasLoader : MonoBehaviour
 {
+    GameObject pause;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class CanvasLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // destroy pause menu after summit
+        if(SceneManager.GetActiveScene().buildIndex == 21)
+        {
+            pause = GameObject.Find("PauseMenu");
+            Destroy(pause);
+        }
     }
 }
